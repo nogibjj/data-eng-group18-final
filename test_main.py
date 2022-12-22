c71858413277ba78b8166ee50e7b70cb4e6e4a16
kvanme01 <kristi.vanmeter@duke.edu>
2022-12-22 18:32:42 -0500

    test

--- test_main.py
+++ test_main.py
@@ -18,7 +18,7 @@ from fastapi.testclient import TestClient
 from main import app
 
 
-@app.get("/")
+@app.get("/test")
 async def test_read_main():
     return {"msg": "Test message"}
 
@@ -27,6 +27,6 @@ client = TestClient(app)
 
 
 def test_read_main():
-    response = client.get("/")
+    response = client.get("/test")
     assert response.status_code == 200
     assert response.json() == {"msg": "Test message"}
