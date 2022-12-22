4bd62f6985bcf39c05cf7ebcff0c6ce2b7de094a
kvanme01 <kristi.vanmeter@duke.edu>
2022-12-22 18:10:47 -0500

    added API testing

--- test_main.py
+++ test_main.py
@@ -8,3 +8,25 @@ df = pd.read_csv("cost_of_living.csv")
 def test_df_ingestion():
     """Test that dataframe was properly ingested"""
     assert (df.shape[0] == 4956) & (df.shape[1] == 58)
+
+
+"""API Testing"""
+
+
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from main import app
+
+
+@app.get("/")
+async def test_read_main():
+    return {"msg": "Test message"}
+
+
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Test message"}
